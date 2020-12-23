0c4dcfd0630199801334749a894358524aad2303
chenjunxue <1523825571@qq.com>
2020-12-23 20:37:43 +0800

    2020-12-17-笔记整理5 AsiHacker

---
+++ linux杂记/基础知识/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/17 20:40
+# @Author  : AsiHacker
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @notice  : True masters always have the heart of an apprentice.
