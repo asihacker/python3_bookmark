fc1d0d651f6a6ba8e2a3c2316c214da70c8004b4
chenjunxue <1523825571@qq.com>
2021-02-19 15:17:29 +0800

    20210219备份

---
+++ git相关/github技巧.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/2/19 13:56
+# @Author  : AsiHacker
+# @File    : github技巧.py
+# @Software: PyCharm
+# @notice  : True masters always have the heart of an apprentice.
+# https://github1s.com/nickliqian/cnn_captcha 在地址中github->github1s 可以打开在线
